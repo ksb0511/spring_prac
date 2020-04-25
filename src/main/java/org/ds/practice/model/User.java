47f2cccd28d9d87879864aa5107470edf27f2085
subeen Kang <soobin796@gmail.com>
2020-04-25 10:23:12 +0900

    Add files via upload

---
+++ src/main/java/org/ds/practice/model/User.java
@@ -0,0 +1,15 @@
+package org.ds.practice.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    //public static String name;
+    //private String name;
+
+    private int user_idx;
+    private String name;
+    private String part;
+
+
+}
